bf59c88191ca0eb993b36fe8236daec28ac1f4df
Sirawit Pongnakintr <s6007589@mwit.ac.th>
2020-03-30 14:11:15 +0700

    Debug onload

--- src/canvas.ts
+++ src/canvas.ts
@@ -8,21 +8,21 @@ let height = document.getElementById('canvas-root').offsetHeight;
 let scene = new THREE.Scene();
 //scene.background = new THREE.Color("white");
 let camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer( {antialias : true} );
+let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
 document.getElementById('canvas-root').appendChild(renderer.domElement);
 
-THREE.DefaultLoadingManager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
-    console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-    if(itemsLoaded > 1) document.getElementById('main-line').classList.remove('expandLeft-step' + (itemsLoaded-1).toString());
+THREE.DefaultLoadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
+    console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+    if (itemsLoaded > 1) document.getElementById('main-line').classList.remove('expandLeft-step' + (itemsLoaded - 1).toString());
     document.getElementById('main-line').classList.add('expandLeft-step' + itemsLoaded.toString());
-};
-THREE.DefaultLoadingManager.onLoad = function () {
-    console.log( 'Loading Complete!');
-    document.getElementById('canvas-root').classList.add("fadeIn");
-    document.querySelectorAll('section').forEach((el) => {
-        el.classList.add('enabled');
-    });
+    if (itemsLoaded === 7) {
+        console.log('Loading Complete!');
+        document.getElementById('canvas-root').classList.add("fadeIn");
+        document.querySelectorAll('section').forEach((el) => {
+            el.classList.add('enabled');
+        });
+    }
 };
 
 interface Fodeld {
@@ -37,40 +37,40 @@ interface Fodeld {
 }
 
 const fodeld = new THREE.Group();
-fodeld.rotation.x = 3*Math.PI/2 + Math.PI/6;
-fodeld.rotation.z = Math.PI/4;
+fodeld.rotation.x = 3 * Math.PI / 2 + Math.PI / 6;
+fodeld.rotation.z = Math.PI / 4;
 loadPart('Body', 0xC0C0C0).then((part) => fodeld.add(part)).then(() =>
-loadPart('Propeller1')).then((part) => fodeld.add(part)).then(() =>
-loadPart('Propeller2')).then((part) => fodeld.add(part)).then(() =>
-loadPart('Propeller3')).then((part) => fodeld.add(part)).then(() =>
-loadPart('Propeller4')).then((part) => fodeld.add(part)).then(() =>
-loadPart('Dropper1', 0xF32013)).then((part) => fodeld.add(part)).then(() =>
-loadPart('Dropper2', 0xF32013)).then((part) => fodeld.add(part))
-.then(() => {
-    scene.add(fodeld);
-    let bbox = new THREE.Box3().setFromObject(fodeld);
-    const dx = bbox.max.x - bbox.min.x;
-    const dy = bbox.max.y - bbox.min.y;
-    const objScale = 1 / Math.max(dx, dy);
-    fodeld.scale.multiplyScalar(objScale);
-    fodeld.position.set(0,0,0);
-    bbox = new THREE.Box3().setFromObject(fodeld);
-    bbox.getCenter(fodeld.position);
-    fodeld.position.multiplyScalar(-1);
-    console.log(fodeld);
-})
-.then(() => {
-    getChild(fodeld, "Dropper1", (child) => {
-        child.position.x -= 10;
-        child.position.y -= 10;
-        console.log(child.position);
-    });
-    getChild(fodeld, "Dropper2", (child) => {
-        child.position.x += 10;
-        child.position.y += 10;
-        console.log(child.position);
-    });
-})
+    loadPart('Propeller1')).then((part) => fodeld.add(part)).then(() =>
+        loadPart('Propeller2')).then((part) => fodeld.add(part)).then(() =>
+            loadPart('Propeller3')).then((part) => fodeld.add(part)).then(() =>
+                loadPart('Propeller4')).then((part) => fodeld.add(part)).then(() =>
+                    loadPart('Dropper1', 0xF32013)).then((part) => fodeld.add(part)).then(() =>
+                        loadPart('Dropper2', 0xF32013)).then((part) => fodeld.add(part))
+    .then(() => {
+        scene.add(fodeld);
+        let bbox = new THREE.Box3().setFromObject(fodeld);
+        const dx = bbox.max.x - bbox.min.x;
+        const dy = bbox.max.y - bbox.min.y;
+        const objScale = 1 / Math.max(dx, dy);
+        fodeld.scale.multiplyScalar(objScale);
+        fodeld.position.set(0, 0, 0);
+        bbox = new THREE.Box3().setFromObject(fodeld);
+        bbox.getCenter(fodeld.position);
+        fodeld.position.multiplyScalar(-1);
+        console.log(fodeld);
+    })
+    .then(() => {
+        getChild(fodeld, "Dropper1", (child) => {
+            child.position.x -= 10;
+            child.position.y -= 10;
+            console.log(child.position);
+        });
+        getChild(fodeld, "Dropper2", (child) => {
+            child.position.x += 10;
+            child.position.y += 10;
+            console.log(child.position);
+        });
+    })
 
 let hemLight = new THREE.HemisphereLight(0xFFFFFF, 0x555555, 1);
 scene.add(hemLight);
@@ -106,14 +106,14 @@ window.addEventListener('resize', () => {
     width = document.getElementById('canvas-root').offsetWidth;
     height = document.getElementById('canvas-root').offsetHeight;
     const r = width / height;
-	const degToRad = (deg : number) => deg * Math.PI / 180;
-	const radToDeg = (rad : number) => rad * 180 / Math.PI;
-	const computeHorizontalFOV = (fov : number, aspectRatio : number) => 2*radToDeg(Math.asin(aspectRatio*Math.sin(degToRad(fov/2))));
-	if(computeHorizontalFOV(50, r) < 50){
-		camera.fov = 2*radToDeg(Math.asin(Math.sin(degToRad(25))/r));
-	}else{
-		camera.fov = 50;
-	}
+    const degToRad = (deg: number) => deg * Math.PI / 180;
+    const radToDeg = (rad: number) => rad * 180 / Math.PI;
+    const computeHorizontalFOV = (fov: number, aspectRatio: number) => 2 * radToDeg(Math.asin(aspectRatio * Math.sin(degToRad(fov / 2))));
+    if (computeHorizontalFOV(50, r) < 50) {
+        camera.fov = 2 * radToDeg(Math.asin(Math.sin(degToRad(25)) / r));
+    } else {
+        camera.fov = 50;
+    }
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
     renderer.setSize(width, height);
